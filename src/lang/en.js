ae7d4d01834bb81e99fec12748697e27e9cf3f2f
tonysoman0203 <somankit2000@gmail.com>
2020-03-04 19:13:42 +0800

    feat: fork initial commit

---
+++ src/lang/en.js
@@ -0,0 +1,15 @@
+// English
+const lang = {
+    contact: 'Contact',
+    born: 'Born',
+    bornIn: 'in',
+    experience: 'Experience',
+    education: 'Education',
+    skills: 'Skills',
+    projects: 'Projects',
+    contributions: 'Contributions',
+    about: 'About me',
+    salary: 'Expected Salary',
+    available: 'Availability'
+};
+export default lang;
